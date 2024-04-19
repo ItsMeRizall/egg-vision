0c77646828526d005994e73fed0dfdc27e0af93a
ItsMeRizall <yoppy.sr2@gmail.com>
2024-04-19 13:01:01 +0700

    add dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,15 @@ import Home from "./pages/Home";
 import Analytic from "./pages/Analytic";
 import Admin from "./pages/Admin/Admin";
 import ResultAnalis from "./pages/ResultAnalis";
+import LandingPage from "./pages/LandingPage";
 
 export default function App() {
   return (
     <div className="bg-[#DCDCDC]">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/result" element={<ResultAnalis />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard/admin" element={<Admin />} />
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import NavLink from '../components/NavLink'
+
+const LandingPage = () => {
+  return (
+    <>
+    <div children className='h-screen px-16 py-4'>
+    <div className='flex bg-transparent justify-end items-center text-[#3E0000]'>
+    <button className="px-9 py-2 border-[#3E0000] border-2 rounded-xl hover:text-white hover:bg-[#3E0000]">Sign In</button>
+    </div>
+    <div className="flex justify-center flex-col h-full text-[#3E0000]">
+                <h1 className="text-8xl font-extrabold">EGG VISION</h1>
+                <p className="text-2xl mt-4">we make a technology to simplify and speed up egg quality control</p>
+            </div>
+    </div>
+    </>
+  )
+}
+
+export default LandingPage
